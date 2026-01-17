34b319ed0c72b6aa6a8bc997b8ebde5df2a8e117
UditNarain <uditnarain1600@gmail.com>
2026-01-17 18:48:50 +0530

    Applying nlp to improve extraction

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 import streamlit as st
+import spacy
+from spacy.matcher import PhraseMatcher
 
 st.title("Job Description Skill Extractor")
-
+skills_db = ["python", "sql", "excel", "react", "java", "machine learning"]
 jd = st.text_area("Paste job description here")
 
-skills_db = ["python", "sql", "excel", "react", "java", "machine learning"]
+nlp = spacy.load("en_core_web_sm")
+matcher = PhraseMatcher(nlp.vocab, attr="LOWER")
+patterns = [nlp.make_doc(skill) for skill in skills_db]
+matcher.add("SKILLS", patterns)
+
+
+
 
 if st.button("Extract Skills"):
-    found = []
-    text = jd.lower()
-    for skill in skills_db:
-        if skill in text:
-            found.append(skill.title())
-    st.write(found if found else "No skills found")
+    doc=nlp(jd)
+    matches = matcher(doc)
+    found = sorted(doc[start:end].text for match_id, start, end in matches)
+    if found:
+        st.subheader("Extracted Skills:")
+        for skill in found:
+            st.write(f"- {skill}")    
+    else:
+        st.write("No skills found.")
 
